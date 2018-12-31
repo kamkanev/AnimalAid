fe3b23fe3f8e0331f8aa07b6be701366697fd4aa
kamen <kamkanev@gmail.com>
2018-12-31 16:39:37 +0200

    make it better

---
+++ app.js
@@ -0,0 +1,170 @@
+var express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var sessions = require('express-session');
+var upload = multer({
+  dest: 'uploads/' // this saves your file into a directory called "uploads"
+}); 
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var session;
+
+var signali = [];
+var users = [
+{username: 'admin', password: 'admin'},
+{username: 'pesho', password: '1234'},
+{username: 'gosho', password: '01234'}
+];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(sessions({
+  secret: '^%^RTfgVuyigYReT%&^$#%*&Rd',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.get('/', (req, res) => {
+  session = req.session;
+  if(session.uniqueID){
+    res.redirect('/redirects')
+  }
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/upload', (req, res) => {
+
+session = req.session;
+  if(!session.uniqueID){
+    res.end("Unauthorized access");
+  }
+  res.sendFile(__dirname + '/upload.html');
+});
+app.get('/karta', (req, res) => {
+  if(!session.uniqueID){
+    res.end("Unauthorized access");
+  }
+  res.sendFile(__dirname + '/map.html');
+});
+app.get('/main.js', (req, res) => {
+  res.sendFile(__dirname + '/main.js');
+});
+app.get('/main2.js', (req, res) => {
+  res.sendFile(__dirname + '/main2.js');
+});
+app.get('/style.css', (req, res) => {
+  res.sendFile(__dirname + '/style.css');
+});
+app.get('/Style.css', (req, res) => {
+  res.sendFile(__dirname + '/Style.css');
+});
+app.get('/login', (req, res) => {
+  session = req.session;
+  if(session.uniqueID){
+    res.redirect('/redirects')
+  }
+  res.sendFile(__dirname + '/login.html');
+});
+
+app.post('/login', function(req, res){
+
+  session = req.session;
+  if(session.uniqueID){
+    res.redirect('/redirects')
+  }
+
+  for(var i=0; i<users.length; i++){
+    if(req.body.username == users[i].username && req.body.password == users[i].password){
+      session.uniqueID = req.body.username;
+      //console.log(session.uniqueID , req.body);
+   }
+  }
+  res.redirect('/redirects');
+
+});
+
+app.post('/signup', function(req, res){
+
+  for (var i = users.length - 1; i >= 0; i--) {
+    if(users[i].username == req.body.username){
+      //res.render('ViewMode', {data: req.body});
+    }
+  }
+  // console.log(req);
+  res.redirect('/login');
+
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/login');
+});
+
+app.get('/redirects', (req, res) => {
+  session = req.session;
+  if(session.uniqueID){
+    res.redirect('/karta');
+  }else{
+    res.redirect('/login')
+    //res.send(req.session.uniqueID +' not found <a href="/logout">log off</a>');
+  }
+});
+
+
+app.use('/uploads', express.static(__dirname + '/uploads')); 
+
+// It's very crucial that the file name matches the name attribute in your html
+app.post('/upload', upload.single('file-to-upload'), (req, res) => {
+
+  if(req.body.x == ''){
+    res.redirect('/upload');
+//     io.on('connection', function(socket){
+
+//       socket.emit("coordsP", "Error skupa!!!");
+  
+// });
+  }else{
+  res.redirect('/karta');
+  if(req.file == undefined){
+    signali.push({
+      koi: req.session.uniqueID,
+    name: req.body.animal,
+    file: "/uploads/dog-paw.jpg",
+    coords: {lat: req.body.x, lng: req.body.y}, // още статове за кръга
+    r: parseInt(req.body.rad),
+    text: req.body.problem
+  });
+    console.log("loading default image");
+  }else{
+  signali.push({
+    koi: req.session.uniqueID,
+  	name: req.body.animal,
+  	file: req.file.path,
+  	coords: {lat: req.body.x, lng: req.body.y}, // още статове за кръга
+    r: parseInt(req.body.rad),
+  	text: req.body.problem
+  });
+  
+}
+//console.log(signali);
+
+	    io.emit("signal", signali[signali.length-1]);
+
+  }
+  // console.log(req.file, req.body);
+  // s push da vkaram vsichko v signali[]
+  
+});
+
+io.on('connection', function(socket){
+
+	    socket.emit("signali", signali);
+	
+});
+
+http.listen(3000, function(){
+	console.log("Server started at port 3000");
+});
---
+++ main.js
@@ -0,0 +1,88 @@
+var socket = io();
+var mymap = L.map('mapid').setView([42.698334, 23.319941], 13);// 42 - lat, 23 - lng
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia2FtY2hvMTEzIiwiYSI6ImNqcGd5ZmZtMjA0eG4zcXF3dHRlb3pmZHQifQ.s_z5folvJO7-ybvZqy0WvA', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(mymap);
+	
+	var popup = L.popup();
+	var markers = [
+	L.marker([42.698204, 23.343312]),
+	L.marker([42.698004, 23.343018]),
+	L.marker([42.699104, 23.343928]),
+	L.marker([42.697404, 23.344188])
+	], marker;
+
+	socket.on("signal", function(sig){
+		//markers = [];
+			markers.push(L.circle([sig.coords.lat, sig.coords.lng], {
+    color: 'red',//otvun
+    fillColor: '#f03',//vutre
+    fillOpacity: 0.3,
+    radius: sig.r
+}).bindPopup("<h2 style='color: blue;'>"+sig.name+"</h2><img style='width: 100%; max-width: 400px; height: auto;' src="+sig.file+"><p>"+sig.text+"</p><p>"+sig.koi+"</p>"));
+//console.log(sig[i]);
+//console.log("1");
+	});
+
+	socket.on("signali", function(sig){
+		markers = [];
+		for (var i = sig.length - 1; i >= 0; i--) {
+			markers.push(L.circle([sig[i].coords.lat, sig[i].coords.lng], {
+    color: 'red',//otvun
+    fillColor: '#f03',//vutre
+    fillOpacity: 0.3,
+    radius: sig[i].r
+}).bindPopup("<h2 style='color: blue;'>"+sig[i].name+"</h2><img style='width: 100%; max-width: 400px; height: auto;' src="+sig[i].file+"><p>"+sig[i].text+"</p><p>"+sig[i].koi+"</p>"));
+//console.log(sig[i]);
+		}
+//console.log("1");
+	});
+
+	socket.on("coordsP", function(msg){
+		//document.getElementById('msg').value = msg;
+		document.getElementById('msg').style.display = 'block';
+	});
+
+function onMapClick(e) {
+	if(marker != undefined){
+		mymap.removeLayer(marker);
+		}
+        //marker = L.marker(e.latlng).addTo(mymap);
+        marker = L.circle(e.latlng, {
+    color: '#6EBE20',//otvun
+    fillColor: '#73D216',//vutre
+    fillOpacity: 0.3,
+    radius: parseInt(document.getElementById("mySelect").value)
+}).addTo(mymap);
+marker.bindPopup("<h2 style='color: blue;'>Kude sum???</h2>");
+document.getElementById('x').value = e.latlng.lat;
+document.getElementById('y').value = e.latlng.lng;
+        //markers.push(marker);
+}
+
+mymap.on('click', onMapClick);
+
+function upImg() {
+	document.getElementById('file-to-upload').style.display = "block";
+}
+
+function imgDis(){
+		document.getElementById('file-to-upload').style.display = "none";
+}
+
+function update() {
+
+	
+	for(var i=0; i<markers.length; i++){
+			markers[i].addTo(mymap);
+		}
+	
+}
+  setInterval(function(){
+		update();
+	  }, 100);
