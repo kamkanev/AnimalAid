55c1e411ade6adfcba313f72fee67175b0a7767d
kamen <kamkanev@gmail.com>
2018-12-31 16:44:35 +0200

    make it better

--- app.js
+++ app.js
@@ -165,6 +165,6 @@ io.on('connection', function(socket){
 	
 });
 
-http.listen(3000, function(){
+http.listen(process.env.PORT || 3000, function(){
 	console.log("Server started at port 3000");
 });
